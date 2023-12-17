781f8af4c95bda5a46cc7cb1a04c1fdc63470760
ketan <professional.ketankj@gmail.com>
2023-12-17 17:22:37 +0530

    switch case basics

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,24 @@
+
+month =3;
+switch (month){
+    case 1:
+        console.log("January");
+        break;//break is neccessary otherwise whole code below it inside switch will also get executed.
+    case 2:
+        console.log("Feb");
+       // break;
+        
+    case 3:
+        console.log("march");
+       // break;
+    case 4:
+        console.log("April");
+       // break;
+    case 5:
+        console.log("May");
+        //break;
+    default:
+        console.log("December");
+        break;
+}
+
