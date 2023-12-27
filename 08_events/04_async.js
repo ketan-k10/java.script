ba802aad1c8d1607d89b85dd8a2e34e58aca753c
ketan <professional.ketankj@gmail.com>
2023-12-27 23:05:48 +0530

    async theory

---
+++ 08_events/04_async.js
@@ -0,0 +1,47 @@
+//  this is not from chai aur javascript
+// from async js video of sheriyans coding school.
+
+// SO LETS START NOW
+
+// # Q1. sync and async kya hota hai ?
+// # Ans:- sync matlab ek k baad dusra hoga,jabtak ek command complete naa ho, dusra shuru nhi hoga.
+//         async matlab saare ek sath shuru kardo, jiska answer pahle aajaye uska jawab dedena.
+
+// # Q2. Kaise pata chalta hai ki hum sync code likh rahe ya async ?
+// # Ans :-    
+//          if you are using any of the following then your code is asynchronous.
+//           @ 1. setTimeout
+//           @ 2. setInterval
+//           @ 3. promises
+//           @ 4. fetch
+//           @ 5. axios
+//           @ 6. XMLHttpRequest.
+
+
+// async ka matlab h do kaam ek saath hona. but js async nhi h, js single thread h.
+
+// # Q3. async js h kya ?
+// # Ans :-    kai baar aapka final code depended hota h kisi aur ke server par, is case mein humein nahi pata hota ki answer uske  server se kab laut kar aayega,
+//             to hum kya nhi kr sakte is writing sync code, isse nipatne ke liye humlog async code likh dete h taki blocking na hoand time waste na ho 
+//              and jab bhi answer aaye hmare answer k respect m chalne wala code chal jaye.
+
+
+
+// request/async code likhne ke liye use karenge 
+
+//           @ 1. setTimeout
+//           @ 2. setInterval
+//           @ 3. promises
+//           @ 4. fetch
+//           @ 5. axios
+//           @ 6. XMLHttpRequest.
+
+
+//  jab inse answer miljayega usko chalane k liye use karenge 
+
+//           @ 1. callbacks
+//           @ 2. then catch
+//           @ 3. async await
+
+
+
